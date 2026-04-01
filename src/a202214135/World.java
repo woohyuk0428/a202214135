b293df555c82d525065072c9ba7fdf4c3030d6fa
woohyuk0428 <whkang03@gmail.com>
2026-04-01 11:24:01 +0900

    five

--- src/a202214135/World.java
+++ src/a202214135/World.java
@@ -5,6 +5,11 @@ public class World {
   public static void run() {
     System.out.println("202214135");
   }
+  
+  public static void run2() {
+    System.out.println("hello world");
+  }
+
 
 }
 
